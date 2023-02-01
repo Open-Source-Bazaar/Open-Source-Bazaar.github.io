507dfd707931d6dc06cf82fa6d015894a08afa72
stevending1st <stevending1st@163.com>
2023-02-01 15:36:50 +0800

    feat: add manifest

--- next.config.js
+++ next.config.js
@@ -1,14 +1,22 @@
 /** @type {import('next').NextConfig} */
 const withMDX = require('@next/mdx')({
-  options: {
-    remarkPlugins: [],
-    rehypePlugins: [],
-    providerImportSource: '@mdx-js/react',
-  },
-  extension: /\.mdx?$/,
-});
+    options: {
+      remarkPlugins: [],
+      rehypePlugins: [],
+      providerImportSource: '@mdx-js/react',
+    },
+    extension: /\.mdx?$/,
+  }),
+  withPWA = require('next-pwa')({
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === 'development',
+  });
 
-module.exports = withMDX({
-  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
-  reactStrictMode: true,
-});
+module.exports = withPWA(
+  withMDX({
+    pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+    reactStrictMode: true,
+  }),
+);
