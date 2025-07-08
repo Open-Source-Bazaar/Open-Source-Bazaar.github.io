30a842b33c7cfc2f9af20f8da6855846259337a4
TechQuery <shiy2008@gmail.com>
2025-07-09 01:58:18 +0800

    [refactor] make Next.js i18n smoother with MobX-i18n 0.7 [refactor] rewrite Lark back-end API with Koa middlewares [add] Main Navigator & Lark Image components [migrate] upgrade ESLint configuration & Lark notification from the Upstream Scaffold [optimize] upgrade to PNPM 10 & other latest Upstream packages

---
+++ components/Navigator/MainNavigator.tsx
@@ -0,0 +1,64 @@
+import { observer } from 'mobx-react';
+import dynamic from 'next/dynamic';
+import { useRouter } from 'next/router';
+import { FC, useContext } from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+
+import { i18n, I18nContext } from '../../models/Translation';
+
+const LanguageMenu = dynamic(() => import('./LanguageMenu'), { ssr: false });
+
+export type MenuItem = Record<'href' | 'name', string>;
+
+const topNavBarMenu = ({ t }: typeof i18n): MenuItem[] => [
+  { href: '/article/about', name: t('about') },
+  { href: '/article/history', name: t('history') },
+  { href: '/article/code-of-conduct', name: t('code_of_conduct') },
+  { href: '/article/join-us', name: t('join_us') },
+  {
+    href: '/article/open-collaborator-award',
+    name: t('open_collaborator_award'),
+  },
+  {
+    href: 'https://github.com/Open-Source-Bazaar/Git-Hackathon-scaffold',
+    name: t('hackathon'),
+  },
+];
+
+export interface MainNavigatorProps {
+  menu?: MenuItem[];
+}
+
+export const MainNavigator: FC<MainNavigatorProps> = observer(({ menu }) => {
+  const { pathname } = useRouter(),
+    i18n = useContext(I18nContext);
+  const { t } = i18n;
+
+  menu ||= topNavBarMenu(i18n);
+
+  return (
+    <Navbar bg="dark" variant="dark" fixed="top" expand="lg">
+      <Container>
+        <Navbar.Brand href="/" className="fw-bolder">
+          {t('open_source_bazaar')}
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav className="me-auto my-2 my-lg-0" navbarScroll>
+            {menu.map(({ href, name }) => (
+              <Nav.Link
+                key={`${href}-${name}`}
+                href={href}
+                className={pathname === `${href}` ? 'fw-bolder text-light' : ''}
+              >
+                {name}
+              </Nav.Link>
+            ))}
+          </Nav>
+
+          <LanguageMenu />
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+});
--- components/PageContent/index.tsx
+++ components/PageContent/index.tsx
@@ -8,22 +8,19 @@ import pageContentStyles from './PageContent.module.scss';
 export type PageContentProps = PropsWithChildren<{}>;
 
 const components = {
-  h1: (props: PropsWithChildren<{}>) => (
+  h1: ({ children }: PropsWithChildren<{}>) => (
     <h1 className="bg-info text-center fw-bolder" style={{ color: 'red' }}>
-      {props?.children}
+      {children}
     </h1>
   ),
 };
 
 export const PageContent: FC<PageContentProps> = ({ children }) => (
   <main
-    className={`flex-fill d-flex flex-column justify-content-start align-items-center bg-secondary bg-gradient text-dark bg-opacity-10 ${styles.main}`}
+    className={`d-flex flex-column justify-content-start align-items-center bg-secondary bg-gradient text-dark bg-opacity-10 ${styles.main}`}
   >
     <Container>
-      <Card
-        body
-        className={`px-5 py-5 lh-base ${pageContentStyles.MDXProvider}`}
-      >
+      <Card body className={`p-5 lh-base ${pageContentStyles.MDXProvider}`}>
         <MDXProvider components={components}>{children}</MDXProvider>
       </Card>
     </Container>
